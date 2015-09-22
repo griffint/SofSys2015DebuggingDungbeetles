23b613a453699e477c066d334b1bce4aa6dbcbc3
griffint <griffin.tschurwald@students.olin.edu>
2015-09-22 12:58:10 -0400

    waveplayback.ino should generate a flat 2.5V  now

--- waveplayback/waveplayback.ino
+++ waveplayback/waveplayback.ino
@@ -9,12 +9,13 @@
 #define LENGTH 256 // Length of the wave lookup table
 byte wave[LENGTH]; // Storage for waveform
 int sensorPin = A0;
+
 //notes for the song zelda - lost woods
 //song starts as FAB FAB FABED BCBGE DEGE
 int f_note_hz = 349;
 int a_note_hz = 440;
 int b_note_hz = 494;
-int e_note_hz = 330;;
+int e_note_hz = 330;
 int d_note_hz = 294;
 int c_note_hz = 262;
 int g_note_hz = 392;
@@ -22,12 +23,23 @@ int g_note_hz = 392;
 void setup() {
 Serial.begin(9600);
 /* Populate the waveform table with a sine wave */
+/*
 for (int i=0; i<LENGTH; i++) { // Step across wave table
    float v = (AMP*sin((PI2/LENGTH)*i)); // Compute value
    wave[i] = int(v+OFFSET); // Store value as integer
+   //Serial.println("Setting the waveform table to the below value");
+   //Serial.println(wave[i]);
+ }
+ /*
+ 
+ /* Or populate it with a flat 2.5V instead 
+     127 should be roughly 2.5V*/
+ for (int i=0; i<LENGTH; i++) {
+   wave[i] = 127;
    Serial.println("Setting the waveform table to the below value");
    Serial.println(wave[i]);
  }
+ 
 
 /****Set timer1 for 8-bit fast PWM output ****/
  pinMode(9, OUTPUT); // Make timer’s PWM pin an output
@@ -35,7 +47,6 @@ for (int i=0; i<LENGTH; i++) { // Step across wave table
  TCCR1A |= (1 << COM1A1); // Pin low when TCNT1=OCR1A
  TCCR1A |= (1 << WGM10); // Use 8-bit fast PWM mode
  TCCR1B |= (1 << WGM12);
-
 /******** Set up timer2 to call ISR ********/
  TCCR2A = 0; // No options in control register A
  TCCR2B = (1 << CS21); // Set prescaler to divide by 8
@@ -55,7 +66,7 @@ int find_ocr2a_from_freq(int frequency)
 }      
 
 void loop() { // Nothing to do!
-//Serial.println(analogRead(sensorPin));
+Serial.println(analogRead(sensorPin));
 }
 
 /******** Called every time TCNT2 = OCR2A ********/
